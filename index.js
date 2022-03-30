d9f973f42b47d3dab80e9c7f1ff3634eb48b367e
Justin Chazen <Justinchazen96@gmail.com>
2022-03-30 15:43:42 -0400

    form kinda working

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ let playerList = document.getElementById('player-list')
 let playerStats = document.getElementById('player-stats')
 let addPlayerForm = document.getElementById('add-player-form')
 let submitButton = document.getElementById('submit')
+let birthCountry = document.createElement('h4')
+let birthCity = document.createElement('h4')
+let birthState = document.createElement('h4')
+let college = document.createElement('h4')
+let position = document.createElement('h4')
+let jersey = document.createElement('h4')
+let height = document.createElement('h4')
+let weight = document.createElement('h4')
 
 
 let request = async () => {
@@ -15,14 +23,6 @@ let request = async () => {
         playerName.addEventListener('click', () => {
             addPlayerForm.remove() //deletes player form
             playerList.remove() //deletes the list of players on the left
-            let birthCountry = document.createElement('h4')
-            let birthCity = document.createElement('h4')
-            let birthState = document.createElement('h4')
-            let college = document.createElement('h4')
-            let position = document.createElement('h4')
-            let jersey = document.createElement('h4')
-            let height = document.createElement('h4')
-            let weight = document.createElement('h4')
             weight.innerText = `Weight: ${element.Weight} pounds`
             height.innerText = `Height: ${parseInt(element.Height / 12)}'${element.Height%12}`
             jersey.innerText = `Jersey #: ${element.Jersey}`
@@ -40,7 +40,28 @@ let request = async () => {
 
 request()
 
-submitButton.addEventListener('click', () => {
-    preventDefault()
+
+// submitButton.addEventListener('click', async (e) => {
+//     e.preventDefault()
+//     let newreq = fetch('https://api.sportsdata.io/v3/nba/scores/json/Players/BKN?key=cd2a129a59b1452abe4d8cfa2ee494ce')
+// })
+
+// submitButton.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     let newPlayer = document.createElement('h2')
+//     newPlayer.innerText = `${newFirstName.value} ${newLastName.value}`
+//    // playerName.innerText = `${newFirstName.value} ${newLastName.value}`
+//     newPlayer.addEventListener('click', () => {
+//         addPlayerForm.remove()
+//         playerList.remove()
+//         birthCountry.innerText = newBirthCountry.value
+//         birthCity.innerText = newBirthCity.value
+//         birthState.innerText = newBirthState.value
+//         height.innerText = newHeight.value
+//         weight.innerText = newWeight.value
+//         playerStats.append(newPlayer, birthCountry, birthCity, birthState, college, position, jersey, height, weight)
+//     })
+//     playerList.append(newPlayer)
+//    // addPlayerForm.reset()
     
-})
+// })
